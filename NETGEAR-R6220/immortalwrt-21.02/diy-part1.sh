ee6915d0cfb3f9237191cddf5f8c052e7030ea21
Xghng999 <1369492@qq.com>
2026-07-13 15:59:26 +0800

    Update diy-part1.sh

--- NETGEAR-R6220/immortalwrt-21.02/diy-part1.sh
+++ NETGEAR-R6220/immortalwrt-21.02/diy-part1.sh
@@ -11,15 +11,8 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 3. (可选) 添加其他常用的依赖源，确保编译不报错
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 # echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
-# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-# 注释掉默认的packages源
-# sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
-
-# 添加修改的packages源
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
